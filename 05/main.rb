83704d77dac4a399dce6b1cdf94dc3d016de9e83
Eleanor Mollett <eleanor.mollett@gmail.com>
2025-12-18 21:13:50 +0000

    Day 5 part 2

--- 05/main.rb
+++ 05/main.rb
@@ -19,6 +19,32 @@ module Day5
       end
     end
 
+    def sort_ranges
+      @fresh_ids.sort{ |a,b| a[0] <=> b[0]}
+    end
+
+    def combine_ranges
+      sorted_ranges = sort_ranges
+      i = 0
+      range_start = sorted_ranges[0][0]
+      range_end = sorted_ranges[0][1]
+      combined_ranges = []
+      until i == (sorted_ranges.length-1) do
+        if sorted_ranges[i][1] >= sorted_ranges[i+1][0]
+          if sorted_ranges[i+1][1] > range_end
+            range_end = sorted_ranges[i+1][1]
+          end
+        else
+          combined_ranges.push([range_start, range_end])
+          range_start = sorted_ranges[i+1][0] 
+          range_end = sorted_ranges[i+1][1]
+        end
+        i += 1
+      end
+      combined_ranges.push([range_start, range_end])
+      combined_ranges
+    end
+
     def check_ingredients
       ingredients = @parsed_input[1]
       ingredients.each do |ingredient|
@@ -32,12 +58,21 @@ module Day5
       end
     end
 
+    def count_fresh
+      combined_ranges = combine_ranges
+      fresh = 0
+      combined_ranges.each do |range|
+        fresh += ((range[1] - range[0]) + 1)
+      end
+      fresh
+    end
+
     def part1
       check_ingredients
     end
 
-    # def part2
-
-    # end
+    def part2
+      count_fresh
+    end
   end
 end
--- spec/05_spec.rb
+++ spec/05_spec.rb
@@ -17,9 +17,21 @@ describe Day5::Main do
       expect(puzzle.fresh_ids).to eq([[3, 5], [10, 14], [16, 20], [12, 18]])
     end
 
+    it 'sorts the ranges' do
+      expect(puzzle.sort_ranges).to eq([[3,5], [10, 14], [12, 18], [16, 20]])
+    end
+
     it 'checks the ingredients to see if they are fresh' do
       puzzle.check_ingredients
       expect(puzzle.fresh_ingredients).to eq([5, 11, 17])
+    end
+
+    it 'combines the ranges' do
+    expect(puzzle.combine_ranges).to eq([[3,5], [10, 20]])
+    end
+
+    it 'counts the fresh ingredients' do
+      expect(puzzle.count_fresh).to eq(14)
     end
   end
 
@@ -30,8 +42,8 @@ describe Day5::Main do
       expect(puzzle.count).to eq(821)
     end
 
-    # it 'returns the answer to part 2' do
-    #   expect(puzzle.part2).to eq
-    # end
+    it 'returns the answer to part 2' do
+      expect(puzzle.part2).to eq(344771884978261)
+    end
   end
 end
